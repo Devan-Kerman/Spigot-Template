7b699a65825e484099b04fd58b481dde5e33f53f
Devan-Kerman <devan@cleverpath.com>
2019-12-24 20:17:00 -0600

    more automation

--- src/main/java/net/devtech/template/Template.java
+++ src/main/java/net/devtech/template/Template.java
@@ -1,13 +1,12 @@
 package net.devtech.template;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Template extends JavaPlugin {
-
+public final class Template extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
-
 	}
 
 	@Override
